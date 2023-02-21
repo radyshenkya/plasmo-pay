9cae018e8070fff26b7b74393861c20ec614d738
radyshenkya <k1ll4h_tv3t@protonmail.com>
2023-02-21 14:18:12 +0700

    Добавил кнопку для открытия диалога перевода

--- src/main/java/net/radyshenkya/plasmopay/PlasmoPayClient.java
+++ src/main/java/net/radyshenkya/plasmopay/PlasmoPayClient.java
@@ -1,11 +1,17 @@
 package net.radyshenkya.plasmopay;
 
+import org.lwjgl.glfw.GLFW;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SignBlock;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.radyshenkya.plasmopay.gui.Payment.PaymentGui;
@@ -13,9 +19,32 @@ import net.radyshenkya.plasmopay.gui.Payment.PaymentScreen;
 
 public class PlasmoPayClient implements ClientModInitializer {
     public static final PlasmoPayConfig config = PlasmoPayConfig.createAndLoad();
+    private static KeyBinding transferGuiKeyBinding;
 
     @Override
     public void onInitializeClient() {
+        registerKeys();
+        registerCallbacks();
+    }
+
+    private void registerKeys() {
+        transferGuiKeyBinding = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.plasmopay.open_transfer_gui",
+                        InputUtil.Type.KEYSYM,
+                        GLFW.GLFW_KEY_U,
+                        "category.plasmopay.plasmopay"));
+    }
+
+    private void registerCallbacks() {
+        // For transfer dialog keybind
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (transferGuiKeyBinding.wasPressed()) {
+                openTransferGui("", 1, "");
+            }
+        });
+
+        // For sign click
         UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
             if (!world.isClient()) {
                 return ActionResult.PASS;
@@ -46,11 +75,15 @@ public class PlasmoPayClient implements ClientModInitializer {
 
         try {
             int parsedAmount = Integer.parseInt(amount);
-            MinecraftClient.getInstance()
-                    .setScreen(new PaymentScreen(new PaymentGui(cardNumber, parsedAmount, comment)));
+            openTransferGui(cardNumber, parsedAmount, comment);
             return true;
         } catch (Exception e) {
             return false;
         }
+    }
+
+    private void openTransferGui(String cardNumber, int amount, String comment) {
+        MinecraftClient.getInstance()
+                .setScreen(new PaymentScreen(new PaymentGui(cardNumber, amount, comment)));
     }
 }
